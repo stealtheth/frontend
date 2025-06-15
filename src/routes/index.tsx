ed0c950689b61dea50b43a01527817cfd81434c9
Faezeh Shakouri <f.shakori.com@gmail.com>
2025-06-15 07:22:51 +0200

    feat: add setupZerodev functionality

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,26 @@ import { createFileRoute } from '@tanstack/react-router'
 import logo from '../logo.svg'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccount, useWalletClient, usePublicClient } from 'wagmi'
+import { sepolia } from 'wagmi/chains';
 import { Button } from '@/components/ui/button'
-import { parseEther, parseAbi, type Abi, decodeEventLog, decodeAbiParameters, parseAbiParameters } from 'viem'
+import { 
+  parseEther, 
+  parseAbi, 
+  decodeAbiParameters, 
+  parseAbiParameters,
+  createPublicClient,
+  http,
+  zeroAddress,
+} from 'viem'
 
 import {
   type Secret,
   hashPrecommitment,
 } from "@0xbow/privacy-pools-core-sdk";
+
+import { KERNEL_V3_1, getEntryPoint } from "@zerodev/sdk/constants"
+import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator"
+import { createKernelAccount, createKernelAccountClient, createZeroDevPaymasterClient } from "@zerodev/sdk"
 
 
 const CONTRACT_ABI = parseAbi([
@@ -17,14 +30,94 @@ const CONTRACT_ABI = parseAbi([
 const ENTRYPOINT_ADDRESS = "0x0e95a2ac10745cad4fdf00394cb6419ed24374f7";
 const DEPOSIT_AMOUNT = parseEther("0.001"); // 0.001 ETH
 
+const ZERODEV_RPC = 'https://rpc.zerodev.app/api/v3/492f3962-eff6-49ea-bddb-916d21cbf7fc/chain/11155111'
+const entryPoint = getEntryPoint("0.7")
+const kernelVersion = KERNEL_V3_1
+
 export const Route = createFileRoute('/')({
   component: App,
 })
 
+
 function App() {
-  const { address } = useAccount();
   const { data: walletClient } = useWalletClient();
   const publicClient = usePublicClient();
+  const { address } = useAccount();
+  const zerodevPublicClient = createPublicClient({
+    transport: http(ZERODEV_RPC),
+    chain: sepolia,
+  });
+  const zerodevPaymaster = createZeroDevPaymasterClient({
+    transport: http(ZERODEV_RPC),
+    chain: sepolia,
+  })
+  
+  const setupZerodev = async () => {
+    if (!walletClient || !address) return;
+    try {
+      // Construct a validator
+      const ecdsaValidator = await signerToEcdsaValidator(zerodevPublicClient, {
+        signer: walletClient, // Pass the wallet client as the signer
+        entryPoint,
+        kernelVersion
+      });
+
+      // Construct a Kernel account
+      const account = await createKernelAccount(zerodevPublicClient, {
+        plugins: {
+          sudo: ecdsaValidator,
+        },
+        entryPoint,
+        kernelVersion
+      });
+
+      // Construct a Kernel account client
+      const kernelClient = createKernelAccountClient({
+        account,
+        chain: sepolia,
+        bundlerTransport: http(ZERODEV_RPC),
+        // Required - the public client
+        client: publicClient,
+        paymaster: {
+            getPaymasterData(userOperation) {
+                return zerodevPaymaster.sponsorUserOperation({userOperation})
+            }
+        },
+      });
+
+      const accountAddress = kernelClient.account.address;
+      console.log("My account:", accountAddress);
+      
+      // Send a UserOp
+      const userOpHash = await kernelClient.sendUserOperation({
+        callData: await kernelClient.account.encodeCalls([{
+          to: zeroAddress,
+          value: BigInt(0),
+          data: "0x",
+        }]),
+      });
+
+      console.log("UserOp hash:", userOpHash)
+      console.log("Waiting for UserOp to complete...")
+    
+      await kernelClient.waitForUserOperationReceipt({
+        hash: userOpHash,
+        timeout: 1000 * 15,
+      })
+    
+      console.log("UserOp completed: https://base-sepolia.blockscout.com/op/" + userOpHash)
+      
+      return kernelClient;
+
+    } catch (error) {
+      console.error("Error in setupZerodev:", error);
+      if (error instanceof Error) {
+        console.error("Error message:", error.message);
+        console.error("Error stack:", error.stack);
+      }
+      throw error;
+    }
+  };
 
   const handleDeposit = async () => {
     try {
@@ -94,6 +187,12 @@ function App() {
       <div>
         <Button onClick={handleDeposit}>
           Deposit
+        </Button>
+        <Button 
+          onClick={setupZerodev}
+          variant="outline"
+        >
+          Setup ZeroDev
         </Button>
       </div>
     </div>
